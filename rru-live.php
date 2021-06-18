8f0837677c5db12a33e042c7d09677f157f24448
David Keyes <david@rfortherestofus.com>
2021-06-18 10:49:26 -0700

    Update live page to add YouTube instead of Twitch

--- rru-live.php
+++ rru-live.php
@@ -9,30 +9,22 @@ get_header();
 <div id="primary" class="content-area bb-grid-cell">
 
 
-  <main class="mt-8 mx-auto max-w-screen-xl px-4 sm:px-6">
+  <main class="max-w-screen-xl px-4 mx-auto mt-8 sm:px-6">
 
 
 
-      <div class="relative max-w-7xl mx-auto lg:grid lg:grid-cols-5 mb-8">
-        <div class="bg-gray-50 px-4 sm:px-6 lg:col-span-2 lg:px-8 xl:pr-12">
+      <div class="relative mx-auto mb-8 max-w-7xl lg:grid lg:grid-cols-5">
+        <div class="px-4 bg-gray-50 sm:px-6 lg:col-span-2 lg:px-8 xl:pr-12">
           <div class="max-w-lg mx-auto">
-          <h1 class="rru-page-title leading-none">
+          <h1 class="leading-none rru-page-title">
           Live
         </h1>
 
           
-        <p class="text-xl text-blue-800 leading-relaxed">
+        <p class="text-xl leading-relaxed text-blue-800">
           I often stream myself livecoding in R. When I go live, it will appear below.
 
 
-      <h2 class="text-gray-600 mt-2 text-base font-normal ml-2">Also watch on
-
-      <a href="https://twitter.com/rfortherest"><i class="mx-2 ml-1 fab fa-lg fa-twitter text-gray-600 hover:text-gray-900"></i></a>
-<a href="https://www.youtube.com/channel/UCH0p67s0WM4-mkMI5l7JSiQ/"><i class="mx-2 fab fa-lg fa-youtube  text-gray-600 hover:text-gray-900"></i></a>
-<a href="https://www.twitch.tv/rfortherestofus"><i class="mx-2 fab fa-lg fa-twitch  text-gray-600 hover:text-gray-900"></i></a>
-
-
-      </h2>
 
 
 
@@ -42,7 +34,7 @@ get_header();
         </p>
           </div>
         </div>
-        <div class="bg-blue-500 px-8 pt-6 pb-2 rounded-lg lg:col-span-3">
+        <div class="px-8 pt-6 pb-2 bg-blue-500 rounded-lg lg:col-span-3">
           <h2 class="text-white">Want to know next time I go live?</h2>
         <?php echo do_shortcode("[ninja_form id=21]"); ?>
           
@@ -52,8 +44,9 @@ get_header();
     
 
 
-    <div class="my-0 w-full overflow-hidden text-base text-blue-800">
-      <iframe src="https://player.twitch.tv/?channel=rfortherestofus&parent=rfortherestofus.com" width="100%" height="500"></iframe>
+    <div class="w-full my-0 overflow-hidden text-base text-blue-800">
+    <iframe width="100%" height="500" src="https://www.youtube.com/embed/live_stream?channel=UCSoUNu8x-HblqqOK_EwdZGw" frameborder="0" allowfullscreen></iframe>
+      
 
     </div>
 
