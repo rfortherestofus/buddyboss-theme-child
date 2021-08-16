091f8cc656e31f96c82219c8d72cb562b5d29990
David Keyes <david@rfortherestofus.com>
2021-08-16 11:58:39 -0700

    Add testimonials to r in 3

--- rru-3months.php
+++ rru-3months.php
@@ -221,12 +221,65 @@ get_header();
 
 
 
+<!-- This example requires Tailwind CSS v2.0+ -->
+<section class="mt-24 bg-gray-800 rounded-md shadow-md">
+  <div class="mx-auto max-w-7xl md:grid md:grid-cols-2 md:px-6 lg:px-8">
+    <div class="px-4 py-12 sm:px-6 md:flex md:flex-col md:py-16 md:pl-0 md:pr-10 md:border-r md:border-gray-900 lg:pr-16">
+      
+      <blockquote class="mt-6 md:flex-grow md:flex md:flex-col">
+        <div class="relative text-lg font-medium text-white md:flex-grow">
+          <svg class="absolute top-0 left-0 w-8 h-8 text-blue-600 transform -translate-x-3 -translate-y-2" fill="currentColor" viewBox="0 0 32 32" aria-hidden="true">
+            <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
+          </svg>
+          <p class="relative text-white">
+          R has enabled me to explore my data more deeply and take control of my data visualizations. I would highly recommend the R in 3 Months program to anyone who wants to learn R.
+          </p>
+        </div>
+        <footer class="mt-8">
+          <div class="flex items-start">
+            <div class="inline-flex flex-shrink-0 border-2 border-white rounded-full">
+              <img class="w-12 h-12 rounded-full" src="../wp-content/themes/buddyboss-theme-child/assets/images/ibrah-sendide.jpeg" alt="Ibrah Sendide">
+            </div>
+            <div class="ml-4">
+              <div class="text-base font-medium text-white">Ibrah Sendide</div>
+              <div class="text-base font-medium text-blue-200">PhD Candidate, University of the Witwatersrand</div>
+            </div>
+          </div>
+        </footer>
+      </blockquote>
+    </div>
+    <div class="px-4 py-12 border-t-2 border-blue-900 sm:px-6 md:py-16 md:pr-0 md:pl-10 md:border-t-0 md:border-l lg:pl-16">
+      
+      <blockquote class="mt-6 md:flex-grow md:flex md:flex-col">
+        <div class="relative text-lg font-medium text-white md:flex-grow">
+          <svg class="absolute top-0 left-0 w-8 h-8 text-blue-600 transform -translate-x-3 -translate-y-2" fill="currentColor" viewBox="0 0 32 32">
+            <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
+          </svg>
+          <p class="relative text-white">
+          On my journey through R in 3 Months, I went from excited to overwhelmed to confident. The way David and Charlie were able to adapt to our different needs and take me from never having coded to producing a final report all in R was amazing
+          </p>
+        </div>
+        <footer class="mt-8">
+          <div class="flex items-start">
+            <div class="inline-flex flex-shrink-0 border-2 border-white rounded-full">
+            <img class="w-12 h-12 rounded-full" src="../wp-content/themes/buddyboss-theme-child/assets/images/lucilla-piccari.jpeg" alt="Lucilla Piccari">
+            </div>
+            <div class="ml-4">
+              <div class="text-base font-medium text-white">Lucilla Piccari</div>
+              <div class="text-base font-medium text-blue-200">Research Physician, Hospital del Mar (Barcelona)</div>
+            </div>
+          </div>
+        </footer>
+      </blockquote>
+    </div>
+  </div>
+</section>
 
 
 
       <!-- This example requires Tailwind CSS v2.0+ -->
       <div class="bg-white">
-        <div class="px-4 py-12 mx-auto mb-16 text-center max-w-7xl sm:px-6 lg:py-32 lg:px-8">
+        <div class="px-4 py-12 mx-auto mb-16 text-center max-w-7xl sm:px-6 lg:pt-24 lg:pb-12 lg:px-8">
           <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
             <span class="block">Ready to get started?</span>
 
