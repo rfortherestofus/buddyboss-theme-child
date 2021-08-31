aabedb75bdad03e10db4bcfa05d047d09f8e3a1a
David Keyes <david@rfortherestofus.com>
2021-08-31 14:17:54 -0700

    Tweaks

--- rru-3months.php
+++ rru-3months.php
@@ -282,7 +282,7 @@ get_header();
                   <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
                 </svg>
                 <p class="relative text-white">
-                  I joined the R for the Rest of Us course with high expectations, and my expectations were blown away. The level of individualized coaching and feedback was extraordinary. I highly recommend making time to take this course!
+                  I joined the R in 3 Months with high expectations, and my expectations were blown away. The level of individualized coaching and feedback was extraordinary. I highly recommend making time to take this course!
                 </p>
               </div>
               <footer class="mt-8">
@@ -309,9 +309,10 @@ get_header();
                 </h2>
                 <div class="flex justify-center mt-8">
                   <div class="inline-flex rounded-md shadow">
-                    <a href="#pricing" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-white bg-blue-600 border border-transparent rounded-md hover:text-white">
-                      Sign Up Now
-                    </a>
+                    
+                    <a href="#pricing" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-blue-700 bg-white border border-transparent rounded-md shadow-sm hover:bg-blue-50 hover:text-blue-700 sm:px-8">
+                  Sign Up Now
+                </a>
                   </div>
 
                 </div>
