f6dcf09902366a7f094b15fa273c74131d7a7157
David Keyes <david@rfortherestofus.com>
2021-08-31 14:12:47 -0700

    Add Catherine testimonial

--- rru-3months.php
+++ rru-3months.php
@@ -256,7 +256,7 @@ get_header();
                   <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
                 </svg>
                 <p class="relative text-white">
-                  On my journey through R in 3 Months, I went from excited to overwhelmed to confident. The way David and Charlie were able to adapt to our different needs and take me from never having coded to producing a final report all in R was amazing
+                  On my journey through R in 3 Months, I went from excited to overwhelmed to confident. The way David and Charlie were able to adapt to our different needs and take me from never having coded to producing a final report all in R was amazing.
                 </p>
               </div>
               <footer class="mt-8">
@@ -273,27 +273,56 @@ get_header();
             </blockquote>
           </div>
         </div>
+        <div class="mx-auto max-w-7xl md:grid md:grid-cols-2 md:px-6 lg:px-8">
+          <div class="px-4 py-12 sm:px-6 md:flex md:flex-col md:py-16 md:pl-0 md:pr-10 md:border-r md:border-gray-900 lg:pr-16">
+
+            <blockquote class="mt-6 md:flex-grow md:flex md:flex-col">
+              <div class="relative text-lg font-medium text-white md:flex-grow">
+                <svg class="absolute top-0 left-0 w-8 h-8 text-blue-600 transform -translate-x-3 -translate-y-2" fill="currentColor" viewBox="0 0 32 32" aria-hidden="true">
+                  <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
+                </svg>
+                <p class="relative text-white">
+                  I joined the R for the Rest of Us course with high expectations, and my expectations were blown away. The level of individualized coaching and feedback was extraordinary. I highly recommend making time to take this course!
+                </p>
+              </div>
+              <footer class="mt-8">
+                <div class="flex items-start">
+                  <div class="inline-flex flex-shrink-0 border-2 border-white rounded-full">
+                    <img class="w-12 h-12 rounded-full" src="../wp-content/themes/buddyboss-theme-child/assets/images/crw.jpeg" alt="Catherine Roller White">
+                  </div>
+                  <div class="ml-4">
+                    <div class="text-base font-medium text-white">Catherine Roller White</div>
+                    <div class="text-base font-medium text-blue-200">CRW Consulting</div>
+                  </div>
+                </div>
+              </footer>
+            </blockquote>
+          </div>
+
+          <div class="px-4 py-12 border-t-2 border-blue-900 sm:px-6 md:py-16 md:pr-0 md:pl-10 md:border-t-0 md:border-l lg:pl-16">
+
+            
+              <div class="px-4 py-12 mx-auto mb-16 text-center max-w-7xl sm:px-6 lg:pt-24 lg:pb-12 lg:px-8">
+                <h2 class="text-3xl font-extrabold tracking-tight text-white sm:text-4xl">
+                  <span class="block">Ready to get started?</span>
+
+                </h2>
+                <div class="flex justify-center mt-8">
+                  <div class="inline-flex rounded-md shadow">
+                    <a href="#pricing" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-white bg-blue-600 border border-transparent rounded-md hover:text-white">
+                      Sign Up Now
+                    </a>
+                  </div>
+
+                </div>
+              </div>
+            
+          </div>
+        </div>
       </section>
 
 
 
-      <!-- This example requires Tailwind CSS v2.0+ -->
-      <div class="bg-white">
-        <div class="px-4 py-12 mx-auto mb-16 text-center max-w-7xl sm:px-6 lg:pt-24 lg:pb-12 lg:px-8">
-          <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            <span class="block">Ready to get started?</span>
-
-          </h2>
-          <div class="flex justify-center mt-8">
-            <div class="inline-flex rounded-md shadow">
-              <a href="#pricing" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-white bg-blue-600 border border-transparent rounded-md hover:text-white">
-                Sign Up Now
-              </a>
-            </div>
-
-          </div>
-        </div>
-      </div>
 
 
       <!-- Gradient Feature Section -->
@@ -530,78 +559,78 @@ get_header();
     </div>
 
     <!-- This example requires Tailwind CSS v2.0+ -->
-<div class="bg-white" id="instructors">
-  <div class="px-4 py-12 mx-auto text-center max-w-7xl sm:px-6 lg:px-8 lg:py-32">
-    <div class="space-y-12">
-      <div class="space-y-5 sm:mx-auto sm:max-w-xl sm:space-y-4 lg:max-w-5xl">
-        <h2 class="text-3xl font-extrabold tracking-tight sm:text-4xl">Instructors</h2>
-        <p class="text-xl text-gray-500">We don't just know how to use R. We know how to <span class="italic">teach</span> R.</p>
-      </div>
-      <ul role="list" class="m-0 mx-auto space-y-16 sm:grid sm:grid-cols-2 sm:gap-16 sm:space-y-0 lg:grid-cols-2 lg:max-w-5xl">
-        <li>
-          <div class="space-y-6">
-            <img class="w-40 h-40 mx-auto rounded-full xl:w-56 xl:h-56" src="../wp-content/themes/buddyboss-theme-child/assets/images/dk-headshot-new.jpg" alt="David Keyes">
-            <div class="space-y-2">
-              <div class="space-y-1 text-lg font-medium leading-6">
-                <h3 class="mb-0">David Keyes</h3>
-                <p class="text-gray-500">he/him</p>
-              </div>
-              <ul role="list" class="flex justify-center ml-0 space-x-5">
-                <li>
-                  <a href="https://twitter.com/dgkeyes/" class="text-gray-400 hover:text-gray-500">
-                    <span class="sr-only">Twitter</span>
-                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
-                      <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
-                    </svg>
-                  </a>
-                </li>
-                <li>
-                  <a href="https://www.linkedin.com/in/dgkeyes/" class="text-gray-400 hover:text-gray-500">
-                    <span class="sr-only">LinkedIn</span>
-                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
-                      <path fill-rule="evenodd" d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z" clip-rule="evenodd" />
-                    </svg>
-                  </a>
-                </li>
-              </ul>
-            </div>
+    <div class="bg-white" id="instructors">
+      <div class="px-4 py-12 mx-auto text-center max-w-7xl sm:px-6 lg:px-8 lg:py-32">
+        <div class="space-y-12">
+          <div class="space-y-5 sm:mx-auto sm:max-w-xl sm:space-y-4 lg:max-w-5xl">
+            <h2 class="text-3xl font-extrabold tracking-tight sm:text-4xl">Instructors</h2>
+            <p class="text-xl text-gray-500">We don't just know how to use R. We know how to <span class="italic">teach</span> R.</p>
           </div>
-        </li>
-        <li>
-          <div class="space-y-6">
-          <img class="w-40 h-40 mx-auto rounded-full xl:w-56 xl:h-56" src="../wp-content/themes/buddyboss-theme-child/assets/images/charlie.jpeg" alt="Charlie Hadley">
-            <div class="space-y-2">
-              <div class="space-y-1 text-lg font-medium leading-6">
-              <h3 class="mb-0">Charlie Hadley</h3>
-                <p class="text-gray-500">she/her</p>
+          <ul role="list" class="m-0 mx-auto space-y-16 sm:grid sm:grid-cols-2 sm:gap-16 sm:space-y-0 lg:grid-cols-2 lg:max-w-5xl">
+            <li>
+              <div class="space-y-6">
+                <img class="w-40 h-40 mx-auto rounded-full xl:w-56 xl:h-56" src="../wp-content/themes/buddyboss-theme-child/assets/images/dk-headshot-new.jpg" alt="David Keyes">
+                <div class="space-y-2">
+                  <div class="space-y-1 text-lg font-medium leading-6">
+                    <h3 class="mb-0">David Keyes</h3>
+                    <p class="text-gray-500">he/him</p>
+                  </div>
+                  <ul role="list" class="flex justify-center ml-0 space-x-5">
+                    <li>
+                      <a href="https://twitter.com/dgkeyes/" class="text-gray-400 hover:text-gray-500">
+                        <span class="sr-only">Twitter</span>
+                        <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
+                          <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
+                        </svg>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="https://www.linkedin.com/in/dgkeyes/" class="text-gray-400 hover:text-gray-500">
+                        <span class="sr-only">LinkedIn</span>
+                        <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
+                          <path fill-rule="evenodd" d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z" clip-rule="evenodd" />
+                        </svg>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
               </div>
-              <ul role="list" class="flex justify-center ml-0 space-x-5">
-                <li>
-                  <a href="https://twitter.com/charliejhadley" class="text-gray-400 hover:text-gray-500">
-                    <span class="sr-only">Twitter</span>
-                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
-                      <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
-                    </svg>
-                  </a>
-                </li>
-                <li>
-                  <a href="https://www.linkedin.com/in/charliejhadley/" class="text-gray-400 hover:text-gray-500">
-                    <span class="sr-only">LinkedIn</span>
-                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
-                      <path fill-rule="evenodd" d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z" clip-rule="evenodd" />
-                    </svg>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </li>
+            </li>
+            <li>
+              <div class="space-y-6">
+                <img class="w-40 h-40 mx-auto rounded-full xl:w-56 xl:h-56" src="../wp-content/themes/buddyboss-theme-child/assets/images/charlie.jpeg" alt="Charlie Hadley">
+                <div class="space-y-2">
+                  <div class="space-y-1 text-lg font-medium leading-6">
+                    <h3 class="mb-0">Charlie Hadley</h3>
+                    <p class="text-gray-500">she/her</p>
+                  </div>
+                  <ul role="list" class="flex justify-center ml-0 space-x-5">
+                    <li>
+                      <a href="https://twitter.com/charliejhadley" class="text-gray-400 hover:text-gray-500">
+                        <span class="sr-only">Twitter</span>
+                        <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
+                          <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
+                        </svg>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="https://www.linkedin.com/in/charliejhadley/" class="text-gray-400 hover:text-gray-500">
+                        <span class="sr-only">LinkedIn</span>
+                        <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
+                          <path fill-rule="evenodd" d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z" clip-rule="evenodd" />
+                        </svg>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </li>
 
-        <!-- More people... -->
-      </ul>
+            <!-- More people... -->
+          </ul>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
 
 
 
