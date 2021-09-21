60ba594ed0df2286936463cd5b2e2a11fccc3010
David Keyes <david@rfortherestofus.com>
2021-09-21 13:18:35 -0700

    Typo

--- single-rin3_feedback.php
+++ single-rin3_feedback.php
@@ -122,7 +122,7 @@ get_header();
 
 
                 <span class="inline mt-4">
-                <a class="text-sm text-gray-400" href="/resources/">Back to all feedback videos -></a>
+                <a class="text-sm text-gray-400" href="/r-in-3-months-feedback-fall-2021/">Back to all feedback videos -></a>
 
                 </span>
 
