04c69717198badc9e8fae49f45fc11adc2d60c78
David Keyes <david@rfortherestofus.com>
2021-09-22 11:51:53 -0700

    Shit is never going to work

--- rru-rin3-feedback.php
+++ rru-rin3-feedback.php
@@ -27,11 +27,11 @@ get_header();
 
   
   
-  <?php echo do_shortcode('[student course_id=18140]'); ?>
+  
     <h1 class="leading-none rru-page-title">
       R in 3 Months Feedback
     </h1>
-    <?php echo do_shortcode("[/student]"); ?>
+
 
 
     <p>
