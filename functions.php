8789d3192d6d855ab18bd4b9b010599696197226
David Keyes <david@rfortherestofus.com>
2021-09-29 07:00:02 -0700

    Autocomplete all virtual WooCommerce orders

--- functions.php
+++ functions.php
@@ -170,17 +170,41 @@ function wc_npr_filter_phone($address_fields)
 }
 
 /**
- * Auto Complete all WooCommerce orders. See https://docs.woocommerce.com/document/automatically-complete-orders/
+ * Autocomplete all virtual WooCommerce orders. See https://quadlayers.com/autocomplete-woocommerce-orders/
  */
-add_action('woocommerce_thankyou', 'custom_woocommerce_auto_complete_order');
-function custom_woocommerce_auto_complete_order($order_id)
-{
-  if (!$order_id) {
-    return;
-  }
 
-  $order = wc_get_order($order_id);
-  $order->update_status('completed');
+add_filter( 'woocommerce_payment_complete_order_status', 'auto_complete_virtual_orders', 10, 3 );
+
+function auto_complete_virtual_orders( $payment_complete_status, $order_id, $order ) {
+$current_status = $order->get_status();
+// We only want to update the status to 'completed' if it's coming from one of the following statuses:
+$allowed_current_statuses = array( 'on-hold', 'pending', 'failed' );
+
+if ( 'processing' === $payment_complete_status && in_array( $current_status, $allowed_current_statuses ) ) {
+
+$order_items = $order->get_items();
+
+// Create an array of products in the order
+$order_products = array_filter( array_map( function( $item ) {
+// Get associated product for each line item
+return $item->get_product();
+}, $order_items ), function( $product ) {
+// Remove non-products
+return !! $product;
+} );
+
+if ( count( $order_products > 0 ) ) {
+// Check if each product is 'virtual'
+$is_virtual_order = array_reduce( $order_products, function( $virtual_order_so_far, $product ) {
+return $virtual_order_so_far && $product->is_virtual();
+}, true );
+
+if ( $is_virtual_order ) {
+$payment_complete_status = 'completed';
+}
+}
+}
+return $payment_complete_status;
 }
 
 // Disable admin emails about user password changes. See https://wordpress.stackexchange.com/questions/206353/disable-email-notification-after-change-of-password
