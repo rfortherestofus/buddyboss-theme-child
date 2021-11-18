ee2a3c906d9b5165bcb24ad14442a36398bad0fb
David Keyes <david@rfortherestofus.com>
2021-11-18 11:59:42 -0800

    Add API food map succes story to consulting page

--- rru-consulting.php
+++ rru-consulting.php
@@ -27,7 +27,7 @@ get_header();
     <div class="md:w-1/2">
 
       <p class="mt-16 text-xl leading-relaxed text-blue-800">
-      High-quality data visualization, workflow improvements, and custom packages to enable your organization to communicate more effectively and more efficiently.
+        High-quality data visualization, workflow improvements, and custom packages to enable your organization to communicate more effectively and more efficiently.
       </p>
 
 
@@ -53,12 +53,12 @@ get_header();
           Data Visualization that Communicates
         </h4>
         <p class="mt-3 text-lg leading-7 text-gray-600">
-          <p class="mt-3 text-lg leading-7 text-gray-600">
-            When the <a href="http://tfff.org/">Ford Family Foundation</a> came to me about working on their first-ever Oregon by the Numbers report, all they knew was they wanted to come up with a way to share key social and economic indicators for each of the 36 counties in the state.</p>
-          <p class="mt-3 text-lg leading-7 text-gray-600">
-            I knew that what they originally presented to me — a series of tables of data — would not garner the attention the topic deserved. So, working with their staff over several months, we developed a visually-appealing report that would clearly communicate strengths and challenges that Oregon’s counties face.</p>
-          <p class="mt-3 text-lg leading-7 text-gray-600">
-            The&nbsp;<a href="https://tfff.org/obtn">full report</a>&nbsp;has been shared widely with policymakers, the media, and others who care about the future of Oregon. </p>
+        <p class="mt-3 text-lg leading-7 text-gray-600">
+          When the <a href="http://tfff.org/">Ford Family Foundation</a> came to me about working on their first-ever Oregon by the Numbers report, all they knew was they wanted to come up with a way to share key social and economic indicators for each of the 36 counties in the state.</p>
+        <p class="mt-3 text-lg leading-7 text-gray-600">
+          I knew that what they originally presented to me — a series of tables of data — would not garner the attention the topic deserved. So, working with their staff over several months, we developed a visually-appealing report that would clearly communicate strengths and challenges that Oregon’s counties face.</p>
+        <p class="mt-3 text-lg leading-7 text-gray-600">
+          The&nbsp;<a href="https://tfff.org/obtn">full report</a>&nbsp;has been shared widely with policymakers, the media, and others who care about the future of Oregon. </p>
         </p>
 
         <div class="mt-5 mb-24 sm:mt-8">
@@ -88,20 +88,20 @@ get_header();
             From One Report, Many
           </h4>
           <p class="mt-3 text-lg leading-7 text-gray-600">
-            Producing one report is a lot of work. Producing tens or even hundreds of reports is completely overwhelming. I help organizations make this daunting task less daunting. Using <a href="https://medium.com/@urban_institute/iterated-fact-sheets-with-r-markdown-d685eb4eafce">parameterized reporting</a>, I help organizations that need to generate reports in bulk. 
+            Producing one report is a lot of work. Producing tens or even hundreds of reports is completely overwhelming. I help organizations make this daunting task less daunting. Using <a href="https://medium.com/@urban_institute/iterated-fact-sheets-with-r-markdown-d685eb4eafce">parameterized reporting</a>, I help organizations that need to generate reports in bulk.
           </p>
           <p class="mt-3 text-lg leading-7 text-gray-600">
-          Need a report for each school in your district? A report for every town in your state? With my help, you can do this without losing your mind.
-</p>
+            Need a report for each school in your district? A report for every town in your state? With my help, you can do this without losing your mind.
+          </p>
           <p class="mt-3 text-lg leading-7 text-gray-600">This is exactly what R for the Rest of Us did in building out 170+ reports for the Partnership for Strong Communities.
-</p>
+          </p>
 
-<div class="mt-5 mb-24 sm:mt-8">
-          <a href="/success-stories/pschousing/" class="px-8 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-blue-600 border border-transparent rounded-md hover:text-white hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue md:py-4 md:text-lg md:px-10">
-            Learn More
-          </a>
+          <div class="mt-5 mb-24 sm:mt-8">
+            <a href="/success-stories/pschousing/" class="px-8 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-blue-600 border border-transparent rounded-md hover:text-white hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue md:py-4 md:text-lg md:px-10">
+              Learn More
+            </a>
 
-        </div>
+          </div>
 
 
 
@@ -109,7 +109,7 @@ get_header();
 
         <div class="relative px-2 -mx-4 sm:mt-10 lg:mt-0 lg:col-start-1 sm:p-10">
 
-        <img class="relative p-4 mx-auto rounded-md shadow lg:ml-8" src="/wp-content/themes/buddyboss-theme-child/assets/images/psc.gif" alt="PSC Housing Reports">
+          <img class="relative p-4 mx-auto rounded-md shadow lg:ml-8" src="/wp-content/themes/buddyboss-theme-child/assets/images/psc.gif" alt="PSC Housing Reports">
 
         </div>
       </div>
@@ -124,35 +124,68 @@ get_header();
         </div>
 
         <h4 class="text-2xl font-extrabold leading-8 tracking-tight text-blue-800 sm:text-3xl sm:leading-9">
-          Consistency = Better Communication
+          A Map for Change in the Asian Pacific Islander Community
         </h4>
         <p class="mt-3 text-lg leading-7 text-gray-600">
-          You may think of R as a tool for data analysis, but it can also be used to make attractive reports that communicate effectively. I helped Colorado-based OMNI Institute by developing a custom R package that enables all of their reports to follow their style guide. Their reports have a clean look and feel, with plots and tables that use OMNI colors and fonts. Instead of spending hours formatting each report the organization makes, OMNI can now simply use this package to ensure their reports look great.
+          When <a href="https://www.intention2impact.com/about">Kathleen Doll of Intention2Impact</a> reached out to R for the Rest of Us for support making an interactive map on food resources for the Asian Pacfic Islander community of San Francisco, she figured that her team would have to do a bunch of manual data entry before we created the map. Working together, we not only made a beautiful interactive map, but we automated the process of collecting data.
         </p>
 
+
+
         <div class="mt-5 mb-24 sm:mt-8">
-          <a href="https://rfortherestofus.github.io/omni/" class="px-8 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-blue-600 border border-transparent rounded-md hover:text-white hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue md:py-4 md:text-lg md:px-10">
+          <a href="/success-stories/api-council-map/" class="px-8 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-blue-600 border border-transparent rounded-md hover:text-white hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue md:py-4 md:text-lg md:px-10">
             Learn More
           </a>
 
         </div>
 
 
+
+
       </div>
 
       <div class="relative hidden mt-10 -mx-4 rounded-md lg:mt-0 sm:block">
 
-        <img class="relative p-4 mx-auto rounded-md shadow" src="/wp-content/themes/buddyboss-theme-child/assets/images/omni.gif" alt="OMNI RMarkdown Template">
+        <img class="relative p-4 mx-auto rounded-md shadow lg:ml-8" src="/wp-content/themes/buddyboss-theme-child/assets/images/apifoodmap.png" alt="API Council Food Assets Map">
 
+      </div>
+    </div>
+
+    <div class="relative mt-12 sm:mt-16 lg:mt-24">
+      <div class="lg:grid lg:grid-flow-row-dense lg:grid-cols-2 lg:gap-8 lg:items-center">
+        <div class="lg:col-start-2">
+          <h4 class="text-2xl font-extrabold leading-8 tracking-tight text-blue-800 sm:text-3xl sm:leading-9">
+            Consistency = Better Communication
+          </h4>
+
+          <p class="mt-3 text-lg leading-7 text-gray-600">
+            You may think of R as a tool for data analysis, but it can also be used to make attractive reports that communicate effectively. I helped Colorado-based OMNI Institute by developing a custom R package that enables all of their reports to follow their style guide. Their reports have a clean look and feel, with plots and tables that use OMNI colors and fonts. Instead of spending hours formatting each report the organization makes, OMNI can now simply use this package to ensure their reports look great.
+          </p>
+
+          <div class="mt-5 mb-24 sm:mt-8">
+            <a href="https://rfortherestofus.github.io/omni/" class="px-8 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-blue-600 border border-transparent rounded-md hover:text-white hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue md:py-4 md:text-lg md:px-10">
+              Learn More
+            </a>
+
+          </div>
+
+
+        </div>
+
+        <div class="relative px-2 -mx-4 sm:mt-10 lg:mt-0 lg:col-start-1 sm:p-10">
+
+          <img class="relative p-4 mx-auto rounded-md shadow" src="/wp-content/themes/buddyboss-theme-child/assets/images/omni.gif" alt="OMNI RMarkdown Template">
+
+        </div>
       </div>
     </div>
 
 
 
-    
 
-    
-    
+
+
+
 
 
   </main><!-- #main -->
