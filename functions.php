6858f6f0a3e65b27ba04f72f0788ca4e073af8c2
David Keyes <david@rfortherestofus.com>
2021-12-30 16:44:17 -0800

    Add 2021 thank you template

--- functions.php
+++ functions.php
@@ -87,6 +87,7 @@ function tailwind_theme_child_scripts_styles()
     | is_page_template('rru-email.php')
     | is_page_template('rru-demystifying.php')
     | is_page_template('rru-1pct.php')
+    | is_page_template('rru-2021-thank-you.php')
     | is_singular( 'resource' )
     | is_singular( 'rin3_feedback' )
   ) {
---
+++ rru-2021-thank-you.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ * Template Name: RRU 2021 Thank You
+ */
+
+get_header();
+?>
+
+<div id="primary" class="content-area bb-grid-cell">
+
+
+  <main class="max-w-screen-xl px-4 mx-auto mt-10 sm:px-6">
+
+    <!-- <div class="py-12 overflow-hidden bg-gray-50">
+      <div class="relative max-w-xl px-4 mx-auto sm:px-6 lg:px-8 lg:max-w-full"> -->
+
+
+
+    <div>
+      <h1 class="leading-none rru-page-title">
+        Thanks for a great 2021
+      </h1>
+
+    </div>
+
+
+    <div class="aspect-w-16 aspect-h-9">
+  <iframe src="https://www.youtube.com/embed/Dvdw6dGbhpU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
+
+
+
+
+
+
+
+  </main><!-- #main -->
+</div><!-- #primary -->
+
+<?php
+get_footer();
