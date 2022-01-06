7b723230bf131555cb4c69bd409ad08d9179cf51
David Keyes <david@rfortherestofus.com>
2022-01-06 08:19:04 -0800

    Update live page

--- rru-live.php
+++ rru-live.php
@@ -10,42 +10,46 @@ get_header();
 
 
   <main class="max-w-screen-xl px-4 mx-auto mt-8 sm:px-6">
+    <div class="px-4 sm:px-6 lg:col-span-2 lg:px-8 xl:pr-12">
+      <h1 class="leading-none rru-page-title">
+        Live
+      </h1>
+    </div>
+
+    <div class="relative mx-auto mb-8 max-w-7xl lg:grid lg:grid-cols-4">
+      <div class="px-4 sm:px-6 lg:col-span-2 lg:px-8 xl:pr-12">
+        <div class="max-w-lg mx-auto">
 
 
 
-      <div class="relative mx-auto mb-8 max-w-7xl lg:grid lg:grid-cols-5">
-        <div class="px-4 sm:px-6 lg:col-span-2 lg:px-8 xl:pr-12">
-          <div class="max-w-lg mx-auto">
-          <h1 class="leading-none rru-page-title">
-          Live
-        </h1>
+          <p class="text-xl leading-relaxed text-blue-800">
+            I regularly live stream on the first Monday of each month at 11:00am Pacific time.
+          </p>
 
+          <p class="text-xl leading-relaxed text-blue-800">
+            I may live stream at other times as well. Enter your email in the form and/or <a href="https://www.youtube.com/channel/UCSoUNu8x-HblqqOK_EwdZGw">subscribe to the R for the Rest of Us channel on YouTube</a> to be notified.
+          </p>
           
-        <p class="text-xl leading-relaxed text-blue-800">
-          I often stream myself livecoding in R. When I go live, it will appear below. 
-        </p>
-        <p class="text-xl leading-relaxed text-blue-800">
-        You can also <a href="https://www.youtube.com/channel/UCSoUNu8x-HblqqOK_EwdZGw">subscribe to the R for the Rest of Us channel on YouTube</a>.
-</p>
 
-        <p class="text-xl leading-relaxed text-blue-800">
-        Code used in live streams is <a href="https://github.com/rfortherestofus/live">available on GitHub</a>.
-</p>
-          </div>
-        </div>
-        <div class="px-8 pt-6 pb-2 bg-blue-500 rounded-lg lg:col-span-3">
-          <h2 class="text-white">Want to know next time I go live?</h2>
-        <?php echo do_shortcode("[ninja_form id=21]"); ?>
-          
-        
+          <p class="text-xl leading-relaxed text-blue-800">
+            Code used in live streams is <a href="https://github.com/rfortherestofus/live">available on GitHub</a>.
+          </p>
         </div>
       </div>
-    
+      <div class="px-8 pt-6 pb-2 bg-blue-500 shadow-lg rounded-sm lg:col-span-2">
+        <h2 class="text-white">Want to know next time I go live?</h2>
+        <?php echo do_shortcode("[ninja_form id=21]"); ?>
+
+
+
+      </div>
+    </div>
+
 
 
     <div class="w-full my-0 overflow-hidden text-base text-blue-800 aspect-w-16 aspect-h-9">
-    <iframe src="https://www.youtube.com/embed/live_stream?channel=UCSoUNu8x-HblqqOK_EwdZGw" frameborder="0" allowfullscreen></iframe>
-      
+      <iframe src="https://www.youtube.com/embed/live_stream?channel=UCSoUNu8x-HblqqOK_EwdZGw" frameborder="0" allowfullscreen></iframe>
+
 
     </div>
 
