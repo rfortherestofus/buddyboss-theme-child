bad6959f1359f6d9bd807dadaba61d29a41e2620
David Keyes <david@rfortherestofus.com>
2022-04-15 15:59:33 -0700

    Add internship template

---
+++ rru-intern-application.php
@@ -0,0 +1,175 @@
+<?php
+/*
+ * Template Name: RRU Intern Application
+ */
+
+get_header();
+?>
+
+<div id="primary" class="content-area bb-grid-cell">
+
+
+    <main class="max-w-screen-md px-4 mx-auto mt-4 sm:px-6">
+
+
+        <div class="relative bg-white overflow-hidden">
+            <div class="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
+                <div class="relative h-full text-lg max-w-prose mx-auto" aria-hidden="true">
+                    <svg class="absolute top-12 left-full transform translate-x-32" width="404" height="384" fill="none" viewBox="0 0 404 384">
+                        <defs>
+                            <pattern id="74b3fd99-0a6f-4271-bef2-e80eeafdf357" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+                                <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+                            </pattern>
+                        </defs>
+                        <rect width="404" height="384" fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)" />
+                    </svg>
+                    <svg class="absolute top-1/2 right-full transform -translate-y-1/2 -translate-x-32" width="404" height="384" fill="none" viewBox="0 0 404 384">
+                        <defs>
+                            <pattern id="f210dbf6-a58d-4871-961e-36d5016a0f49" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+                                <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+                            </pattern>
+                        </defs>
+                        <rect width="404" height="384" fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
+                    </svg>
+                    <svg class="absolute bottom-12 left-full transform translate-x-32" width="404" height="384" fill="none" viewBox="0 0 404 384">
+                        <defs>
+                            <pattern id="d3eb07ae-5182-43e6-857d-35c643af9034" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+                                <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+                            </pattern>
+                        </defs>
+                        <rect width="404" height="384" fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)" />
+                    </svg>
+                </div>
+            </div>
+
+            <div id="notes-on-hiring-junior-person" class="section level1">
+                <div class="max-w-prose mx-auto prose text-xl text-gray-600 leading-8">
+                    <h1>
+                        <span class="block text-base text-center text-blue-600 font-semibold tracking-wide uppercase">Summer 2022</span>
+                        <span class="mt-1 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">R for the Rest of Us Internship</span>
+                    </h1>
+
+                    <div class="my-4">
+                        <img class="rounded-full w-1/3 mx-auto" src="/wp-content/themes/buddyboss-theme-child/assets/images/dk-headshot-new.jpg" alt="David Keyes" />
+                    </div>
+
+                    <p>Hi, I’m David and I run R for the Rest of Us. I’m looking to hire an
+                        intern this summer. I’m doing this because R for the Rest of Us has a
+                        lot of work and we could use some extra hands. And also because I’d like
+                        to support someone early in their career who wants to improve their R
+                        skills and learn about how a consulting business works.</p>
+                    <p>If you’re selected to be the R for the Rest of Us summer intern,
+                        you’ll work directly with me for the most part. I’m a pretty easygoing
+                        guy and decent to work for, I think (I’ll be happy to connect you with
+                        people who have worked for me who can confirm). You may also work
+                        directly with <a href="https://rfortherestofus.com/about/#consultants">R
+                            for the Rest of Us consultants</a> on proejcts. I’ll do my best to make
+                        sure you have a good experience and learn a lot.</p>
+                    <p>I look forward to reading your application!</p>
+                    <p>Cheers,</p>
+                    <p>David</p>
+                </div>
+
+                <hr>
+                <div class="text-md max-w-prose mx-auto prose text-gray-600 leading-8">
+                    <h2>Overview</h2>
+
+                    <p>R for the Rest of Us is looking to hire a paid summer intern. This
+                        fully-remote part-time position is a great opportunity for a student or
+                        early-career professional looking to improve their R skills and learn
+                        about running a business at the same time.</p>
+                    <p>The internship will run for 10 weeks starting whenever the right
+                        candidate is identified. Pay for this position is $30/hour with 10 hours
+                        per week guaranteed (there is a possibility of more hours depending on
+                        projects coming in and intern availability).</p>
+                    <p>I particularly encourage applications from people of diverse
+                        backgrounds throughout the world. There are no limitations on residence
+                        for this position, though the intern will need to be able to have calls
+                        with me during U.S. Pacific time work hours.</p>
+                    <div id="about-us" class="section level2">
+                        <h2>About R for the Rest of Us</h2>
+                        <p>R for the Rest of Us is run remotely by me along with <a href="https://rfortherestofus.com/about/#consultants">several
+                                consultants</a> in the United States and Europe. Our work is in two main
+                            areas: education and consulting.</p>
+                        <p>In education, R for the Rest of Us offers <a href="https://rfortherestofus.com/online-courses">self-paced
+                                courses</a>, a <a href="https://rfortherestofus.com/3months/">three-month cohort-based
+                                program</a>, and <a href="https://rfortherestofus.com/custom-training/">custom trainings for
+                                organizations</a>.</p>
+                        <p>Consulting work (i.e.&nbsp;work where the R for the Rest of Us team does
+                            the coding rather than teaching others how to do it) involves helping
+                            organizations use R to improve their reporting and overall workflow.
+                            Examples of such work include:</p>
+                        <ol style="list-style-type: decimal">
+                            <li><a href="https://rfortherestofus.com/success-stories/ors-impact/">State-level
+                                    reports on 2020 Census outreach efforts</a></li>
+                            <li><a href="https://rfortherestofus.com/success-stories/pschousing/">Connecticut
+                                    housing data profiles</a></li>
+                            <li><a href="https://rfortherestofus.com/success-stories/api-council-map/">Interactive
+                                    map on food resources for the Asian Pacific Islander community of San
+                                    Francisco</a></li>
+                        </ol>
+                    </div>
+                    <div id="what-youll-work-on" class="section level2">
+                        <h2>What You'll Work On</h2>
+                        <p>The summer intern will work on a combination of projects.</p>
+                        <p>The main proejct will be working with David Keyes, R for the Rest of
+                            Us CEO to develop an internal dashboard to track the company’s progress.
+                            In addition, the intern will work on client projects, helping David with
+                            video and podcast production for interviews he’s in the process of
+                            conducting, general marketing work, and more.</p>
+                        <p>I want this experience to be valuable to R for the Rest of Us
+                            (obviously), but also to you. We have a number of projects and I’m happy
+                            to work together to figure out which are the best fit for you to work
+                            on.</p>
+                    </div>
+                    <div id="about-you" class="section level2">
+                        <h2>About You</h2>
+                        <p>The nature of the work at R for the Rest of Us means that we work on
+                            a wide variety of projects. Below is a list of skills we often use in
+                            our work. I don’t expect to find a unicorn who has every single one of
+                            these skills, but the more you have, the more competitive you are likely
+                            to be.</p>
+                        <ol style="list-style-type: decimal">
+                            <li>General familiarity with the tidyverse</li>
+                            <li>Experience with RMarkdown</li>
+                            <li>Experience using ggplot to make high-quality data viz</li>
+                            <li>Experience making static maps with ggplot and interactive maps with
+                                leaflet or similar package</li>
+                            <li>Proficiency with Git and GitHub</li>
+                            <li>General knowledge of HTML + CSS</li>
+                            <li>Experience connecting to APIs to collect data</li>
+                            <li>Self starter who is good at independently managing multiple
+                                simultaneous projects</li>
+                        </ol>
+                        <p>There are a few other skills that I’d love to see someone have:</p>
+                        <ol style="list-style-type: decimal">
+                            <li>Good writing skills</li>
+                            <li>Interest in learning about running a business</li>
+                        </ol>
+                        <p>Please note that you do not need advanced statistical knowledge to
+                            apply for this position. The vast majority of our work involves nothing
+                            more complex than descriptive statistics.</p>
+                    </div>
+                    <div id="application-procedures" class="section level2">
+                        <h2>How to Apply</h2>
+                        <p>To apply, please email me (<a href="mailto:david@rfortherestofus.com" class="email">david@rfortherestofus.com</a>) with the subject line “2022
+                            Summer Internship” and include the following items:</p>
+                        <ol style="list-style-type: decimal">
+                            <li>Cover letter explaining who you are, why you are a good fit for this internship, and what you hope to get out of it.</li>
+                            <li>Resume or link to your LinkedIn profile (something so that I can see
+                                your education and past experience).</li>
+                            <li>Link to your GitHub profile (if you have one) or, alternatively, some way for me to see code you've developed.</li>
+                            <li>An example or two of the data viz work you are most proud of.</li>
+                        </ol>
+                        <p>Applications will be considered on a rolling basis, with the deadline
+                            for submission XXXX.</p>
+                    </div>
+
+                </div>
+            </div>
+
+    </main><!-- #main -->
+</div><!-- #primary -->
+
+<?php
+get_footer();
