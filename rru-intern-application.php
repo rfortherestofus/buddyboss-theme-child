1b6827dbb64060a0fbedc4b4f0f04f70d23b9668
David Keyes <david@rfortherestofus.com>
2022-04-18 11:32:17 -0700

    Typo

--- rru-intern-application.php
+++ rru-intern-application.php
@@ -144,9 +144,10 @@ get_header();
                     <div id="application-procedures" class="section level2">
                         <h2>How to Apply</h2>
                         <p>To apply, please email fill out the form below.</p>
-                        <?php echo do_shortcode("[ninja_form id=31]"); ?>
                         <p>Applications will be considered on a rolling basis, with the deadline
                             for submission May 20, 2022.</p>
+                        <?php echo do_shortcode("[ninja_form id=31]"); ?>
+                        
 
                             
                     </div>
