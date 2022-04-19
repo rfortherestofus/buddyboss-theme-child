eea892cf1448f9ae0e9d3e58b7fd09f0039b0327
David Keyes <david@rfortherestofus.com>
2022-04-19 14:22:21 -0700

    Fix links not working on internship page

--- rru-intern-application.php
+++ rru-intern-application.php
@@ -12,149 +12,145 @@ get_header();
     <main class="max-w-screen-md px-4 mx-auto mt-4 sm:px-6">
 
 
-        <div class="relative bg-white overflow-hidden">
-            <div class="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
 
+
+
+        <div class="max-w-prose mx-auto prose text-xl text-gray-600 leading-8">
+            <h1>
+                <span class="block text-base text-center text-blue-600 font-semibold tracking-wide uppercase">Summer 2022</span>
+                <span class="mt-1 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">R for the Rest of Us Internship</span>
+            </h1>
+
+            <div class="my-4">
+                <img class="rounded-full w-1/3 mx-auto" src="/wp-content/themes/buddyboss-theme-child/assets/images/dk-headshot-new.jpg" alt="David Keyes" />
             </div>
 
-            <div id="notes-on-hiring-junior-person" class="section level1">
-                <div class="max-w-prose mx-auto prose text-xl text-gray-600 leading-8">
-                    <h1>
-                        <span class="block text-base text-center text-blue-600 font-semibold tracking-wide uppercase">Summer 2022</span>
-                        <span class="mt-1 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">R for the Rest of Us Internship</span>
-                    </h1>
+            <p>Hi, I’m David and I run R for the Rest of Us.</p>
+            <p>I'm hiring a summer intern because
+                there are several R for the Rest of Us projects where I need support. But I don't just want this
+                internship to benefit me. I’d also like
+                to support someone early in their career who wants to improve their R
+                skills and learn how to run a business like R for the Rest of Us.</p>
+            <p>If you’re selected to be the R for the Rest of Us summer intern,
+                you’ll work directly with me for the most part. I’m a pretty easygoing
+                guy and decent to work for, I think (I’ll be happy to connect you with
+                people who have worked for me who can confirm). You may also work
+                directly with <a href="https://rfortherestofus.com/about/#consultants">R
+                    for the Rest of Us consultants</a> on projects. I’ll do my best to make
+                sure you have a good experience and learn a lot.</p>
+            <p>I look forward to reading your application!</p>
+            <p>Cheers,</p>
+            <p>David</p>
+        </div>
 
-                    <div class="my-4">
-                        <img class="rounded-full w-1/3 mx-auto" src="/wp-content/themes/buddyboss-theme-child/assets/images/dk-headshot-new.jpg" alt="David Keyes" />
-                    </div>
+        <hr>
+        <div class="text-md max-w-prose mx-auto prose text-gray-600 leading-8">
+            <h2>Overview</h2>
 
-                    <p>Hi, I’m David and I run R for the Rest of Us.</p>
-                    <p>I'm hiring a summer intern because
-                        there are several R for the Rest of Us projects where I need support. But I don't just want this
-                        internship to benefit me. I’d also like
-                        to support someone early in their career who wants to improve their R
-                        skills and learn how to run a business like R for the Rest of Us.</p>
-                    <p>If you’re selected to be the R for the Rest of Us summer intern,
-                        you’ll work directly with me for the most part. I’m a pretty easygoing
-                        guy and decent to work for, I think (I’ll be happy to connect you with
-                        people who have worked for me who can confirm). You may also work
-                        directly with <a href="https://rfortherestofus.com/about/#consultants">R
-                            for the Rest of Us consultants</a> on projects. I’ll do my best to make
-                        sure you have a good experience and learn a lot.</p>
-                    <p>I look forward to reading your application!</p>
-                    <p>Cheers,</p>
-                    <p>David</p>
-                </div>
-
-                <hr>
-                <div class="text-md max-w-prose mx-auto prose text-gray-600 leading-8">
-                    <h2>Overview</h2>
-
-                    <p>R for the Rest of Us is looking to hire a paid summer intern. This
-                        fully-remote part-time position is a great opportunity for a student or
-                        early-career professional looking to improve their R skills and learn
-                        about running a business at the same time.</p>
-                    <p>The internship will run for 10 weeks starting June 1 or whenever the right
-                        candidate is identified. Pay for this position is $30/hour with 10 hours
-                        per week guaranteed (there is a possibility of more hours depending on
-                        projects coming in and intern availability).</p>
-                    <p>I particularly encourage applications from people of diverse
-                        backgrounds throughout the world. There are no limitations on residence
-                        for this position, though the intern will need to speak and write English fluently and
-                        be available for have weekly calls with me during U.S. Pacific time work hours.</p>
-                    <div id="about-us" class="section level2">
-                        <h2>About R for the Rest of Us</h2>
-                        <p>R for the Rest of Us is run remotely by me along with <a href="https://rfortherestofus.com/about/#consultants">several
-                                consultants</a> in the United States and Europe. Our work is in two main
-                            areas: education and consulting.</p>
-                        <p>In education, R for the Rest of Us offers <a href="https://rfortherestofus.com/online-courses">self-paced
-                                courses</a>, a <a href="https://rfortherestofus.com/3months/">three-month cohort-based
-                                program</a>, and <a href="https://rfortherestofus.com/custom-training/">custom trainings for
-                                organizations</a>.</p>
-                        <p>Consulting work (i.e. work where the R for the Rest of Us team does
-                            the coding rather than teaching others how to do it) involves helping
-                            organizations use R to improve their reporting and overall workflow.
-                            Examples of such work include:</p>
-                        <ol class="list-decimal">
-                            <li><a href="https://rfortherestofus.com/success-stories/ors-impact/">State-level
-                                    reports on 2020 Census outreach efforts</a></li>
-                            <li><a href="https://rfortherestofus.com/success-stories/pschousing/">Connecticut
-                                    housing data profiles</a></li>
-                            <li><a href="https://rfortherestofus.com/success-stories/api-council-map/">Interactive
-                                    map on food resources for the Asian Pacific Islander community of San
-                                    Francisco</a></li>
-                        </ol>
-                    </div>
-                    <div id="what-youll-work-on" class="section level2">
-                        <h2>What You'll Work On</h2>
-                        <p>You will work on a combination of projects. The main ones I have in mind are below.</p>
-                        <p>There is some flexibility in terms of which projects you work on.
-                            I want this experience to be valuable to R for the Rest of Us
-                            (obviously), but also to you. I’m happy
-                            to work together to figure out which are the best fit for you to work
-                            on.</p>
-                        <ol class="list-decimal">
-                            <li>The first project will be helping to develop an <span class="font-bold">internal dashboard</span> to track the company’s progress.
-                                This will involve collecting data from a variety of sources before cleaning, analyzing, and reporting on it. 
-                            </li>
-                            <li>You'll also work on <span class="font-bold">consulting projects</span> similar to those above, conducting data collection, cleaning, analysis, visualization, and reporting.</li>
-                            <li>The final project will be assisting me with <span class="font-bold">marketing</span> for R for the Rest of Us.
-                                This will involve helping with a podcast and video series I'm in the process of launching.
-                                It will also involve assistance with marketing for my in-progress book,
-                                <a href="https://rwithoutstatistics.com/">R Without Statistics</a>.
-                        </ol>
-                    </div>
-                    <div id="about-you" class="section level2">
-                        <h2>About You</h2>
-                        <p>The nature of the work at R for the Rest of Us means that we work on
-                            a wide variety of projects. While there are a number of specific skills that will be helpful
-                            for particular projects, there are three main things that will make you competitive
-                            for this position.</p>
-                         
-                            <ol class="list-decimal">
-
-                            <li>General familiarity with the tidyverse, RMarkdown, and data visualization with ggplot</li>
-                            <li>A self-starter who is good at independently managing multiple simultaneous projects</li>
-                            <li>Interest in learning both about the R side of R for the Rest of Us as well as how the business works</li>
-
-                            </ol>
-                            
-                            <p>
-                                There are a few other specific areas that, if you have skills in them, I'd love to know about it,
-                                as they are part of the work we do at R for the Rest of Us. However, as long as I see that you have the skills
-                                above and are open to learning the skills below, I'm happy to help you grow in these areas. 
-                            </p>
-
-                            
-
-                            </p>
-                        <ol class="list-decimal">
-                            <li>High-quality data visualization</li>
-                            <li>Proficiency with Git and GitHub</li>
-                            <li>General knowledge of HTML + CSS</li>
-                            <li>Experience connecting to APIs to collect data</li>
-                            <li>Good communication skills (especially writing)</li>
-                        </ol>
-              
-                        <p>Please note that you do not need advanced statistical knowledge to
-                            apply for this position. The vast majority of our work involves nothing
-                            more complex than descriptive statistics.</p>
-                    </div>
-                    <div id="application-procedures" class="section level2">
-                        <h2>How to Apply</h2>
-                        <p>To apply, please email fill out the form below. Applications will be considered on a rolling basis, with the deadline
-                            for submission May 20, 2022.</p>
-
-                            <p>Have any questions? Email me at david@rfortherestofus.com</p>
-                        <?php echo do_shortcode("[ninja_form id=31]"); ?>
-                        
-
-                            
-                    </div>
-
-                </div>
+            <p>R for the Rest of Us is looking to hire a paid summer intern. This
+                fully-remote part-time position is a great opportunity for a student or
+                early-career professional looking to improve their R skills and learn
+                about running a business at the same time.</p>
+            <p>The internship will run for 10 weeks starting June 1 or whenever the right
+                candidate is identified. Pay for this position is $30/hour with 10 hours
+                per week guaranteed (there is a possibility of more hours depending on
+                projects coming in and intern availability).</p>
+            <p>I particularly encourage applications from people of diverse
+                backgrounds throughout the world. There are no limitations on residence
+                for this position, though the intern will need to speak and write English fluently and
+                be available for have weekly calls with me during U.S. Pacific time work hours.</p>
+            <div id="about-us" class="section level2">
+                <h2>About R for the Rest of Us</h2>
+                <p>R for the Rest of Us is run remotely by me along with <a href="https://rfortherestofus.com/about/#consultants">several
+                        consultants</a> in the United States and Europe. Our work is in two main
+                    areas: education and consulting.</p>
+                <p>In education, R for the Rest of Us offers <a href="https://rfortherestofus.com/online-courses">self-paced
+                        courses</a>, a <a href="https://rfortherestofus.com/3months/">three-month cohort-based
+                        program</a>, and <a href="https://rfortherestofus.com/custom-training/">custom trainings for
+                        organizations</a>.</p>
+                <p>Consulting work (i.e. work where the R for the Rest of Us team does
+                    the coding rather than teaching others how to do it) involves helping
+                    organizations use R to improve their reporting and overall workflow.
+                    Examples of such work include:</p>
+                <ol class="list-decimal">
+                    <li><a href="https://rfortherestofus.com/success-stories/ors-impact/">State-level
+                            reports on 2020 Census outreach efforts</a></li>
+                    <li><a href="https://rfortherestofus.com/success-stories/pschousing/">Connecticut
+                            housing data profiles</a></li>
+                    <li><a href="https://rfortherestofus.com/success-stories/api-council-map/">Interactive
+                            map on food resources for the Asian Pacific Islander community of San
+                            Francisco</a></li>
+                </ol>
             </div>
+            <div id="what-youll-work-on" class="section level2">
+                <h2>What You'll Work On</h2>
+                <p>You will work on a combination of projects. The main ones I have in mind are below.</p>
+                <p>There is some flexibility in terms of which projects you work on.
+                    I want this experience to be valuable to R for the Rest of Us
+                    (obviously), but also to you. I’m happy
+                    to work together to figure out which are the best fit for you to work
+                    on.</p>
+                <ol class="list-decimal">
+                    <li>The first project will be helping to develop an <span class="font-bold">internal dashboard</span> to track the company’s progress.
+                        This will involve collecting data from a variety of sources before cleaning, analyzing, and reporting on it.
+                    </li>
+                    <li>You'll also work on <span class="font-bold">consulting projects</span> similar to those above, conducting data collection, cleaning, analysis, visualization, and reporting.</li>
+                    <li>The final project will be assisting me with <span class="font-bold">marketing</span> for R for the Rest of Us.
+                        This will involve helping with a podcast and video series I'm in the process of launching.
+                        It will also involve assistance with marketing for my in-progress book,
+                        <a href="https://rwithoutstatistics.com/">R Without Statistics</a>.
+                </ol>
+            </div>
+            <div id="about-you" class="section level2">
+                <h2>About You</h2>
+                <p>The nature of the work at R for the Rest of Us means that we work on
+                    a wide variety of projects. While there are a number of specific skills that will be helpful
+                    for particular projects, there are three main things that will make you competitive
+                    for this position.</p>
 
+                <ol class="list-decimal">
+
+                    <li>General familiarity with the tidyverse, RMarkdown, and data visualization with ggplot</li>
+                    <li>A self-starter who is good at independently managing multiple simultaneous projects</li>
+                    <li>Interest in learning both about the R side of R for the Rest of Us as well as how the business works</li>
+
+                </ol>
+
+                <p>
+                    There are a few other specific areas that, if you have skills in them, I'd love to know about it,
+                    as they are part of the work we do at R for the Rest of Us. However, as long as I see that you have the skills
+                    above and are open to learning the skills below, I'm happy to help you grow in these areas.
+                </p>
+
+
+
+                </p>
+                <ol class="list-decimal">
+                    <li>High-quality data visualization</li>
+                    <li>Proficiency with Git and GitHub</li>
+                    <li>General knowledge of HTML + CSS</li>
+                    <li>Experience connecting to APIs to collect data</li>
+                    <li>Good communication skills (especially writing)</li>
+                </ol>
+
+                <p>Please note that you do not need advanced statistical knowledge to
+                    apply for this position. The vast majority of our work involves nothing
+                    more complex than descriptive statistics.</p>
+            </div>
+            <div id="application-procedures" class="section level2">
+                <h2>How to Apply</h2>
+                <p>To apply, please email fill out the form below. Applications will be considered on a rolling basis, with the deadline
+                    for submission May 20, 2022.</p>
+
+                <p>Have any questions? Email me at david@rfortherestofus.com</p>
+                <?php echo do_shortcode("[ninja_form id=31]"); ?>
+
+
+
+
+
+            </div>
+        </div>
     </main><!-- #main -->
 </div><!-- #primary -->
 
