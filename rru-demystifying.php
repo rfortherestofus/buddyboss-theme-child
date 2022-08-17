f5b682991583cb3953e6cc9be84060f274881591
David Keyes <david@rfortherestofus.com>
2022-08-17 14:44:19 -0700

    Add demystifing signup

--- rru-demystifying.php
+++ rru-demystifying.php
@@ -9,43 +9,49 @@ get_header();
 <div id="primary" class="content-area">
 
   <main class="max-w-screen-xl px-4 mx-auto mt-2 ">
-  <div class="relative max-w-md px-4 mx-auto sm:max-w-3xl sm:px-6 lg:px-0 lg:max-w-none lg:py-10">
-            <!-- Testimonial card-->
-            <div class="relative pt-64 pb-10 overflow-hidden shadow-xl rounded-2xl">
-              <img class="absolute inset-0 object-cover w-full h-full" src="../wp-content/themes/buddyboss-theme-child/assets/images/demystifying.svg" alt="Demystifying R">
-              
-              
-              
-            </div>
-          </div>
-        </div>
+    <div class="relative max-w-md px-4 mx-auto sm:max-w-3xl sm:px-6 lg:px-0 lg:max-w-none lg:py-10">
+      <!-- Testimonial card-->
+      <div class="relative pt-64 pb-10 overflow-hidden shadow-xl rounded-2xl">
+        <img class="absolute inset-0 object-cover w-full h-full" src="../wp-content/themes/buddyboss-theme-child/assets/images/demystifying-r.png" alt="Demystifying R">
 
-        <div class="relative max-w-md px-4 mx-auto sm:max-w-3xl sm:px-6 lg:px-0">
-          <!-- Content area -->
-          <div class="pt-12 sm:pt-16 lg:pt-20">
-            <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-              Demystifying R: A Guided Tour
-            </h2>
-            <div class="mt-6 space-y-6 text-gray-500">
-              <p class="text-lg">
-              R is incredibly powerful software, but getting started with it can be confusing. 
-              </p>
-              <p class="text-base leading-7">
-              In this presentation, I discuss reasons why R is worth the effort it takes to learn. These include fast, efficient, and reproducible data analysis, high-quality data visualization, and doing everything from data importing to final reporting in a single tool. 
-              </p>
-              <p class="text-base leading-7">
-              The presentation includes an overview of what R is, how it works, and what it can do for you.
-              </p>
-            </div>
-          
-          
-          </div>
-          </div>
+
+
+      </div>
+    </div>
 </div>
 
-<div class="bg-white">
+<div class="relative max-w-md px-4 mx-auto sm:max-w-3xl sm:px-6 lg:px-0">
+  <!-- Content area -->
+  <div class="pt-12 sm:pt-16 lg:pt-20">
+    <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+      Demystifying R: A Guided Tour
+    </h2>
+    <div class="mt-6 space-y-6 text-gray-500">
+      <p class="text-lg">
+        R is incredibly powerful software, but getting started with it can be confusing.
+      </p>
+      <p class="text-base leading-7">
+        In this presentation, we discuss reasons why R is worth the effort it takes to learn. These include fast, efficient, and reproducible data analysis, high-quality data visualization, and doing everything from data importing to final reporting in a single tool.
+      </p>
+      <p class="text-base leading-7">
+        The presentation includes an overview of what R is, how it works, and what it can do for you.
+      </p>
+    </div>
+
+    <div class="bg-gray-50 text-base leading-7 mt-4 mx-4 rounded-md px-8 py-4">
+      <p class="font-bold">Join us on Thursday, September 8 at 11:00am Pacific time</p>
+      <?php echo do_shortcode("[ninja_form id=32]"); ?>
+    </div>
+
+  </div>
+</div>
+</div>
+
+
+
+<div class="bg-white hidden">
   <div class="px-4 py-12 mx-auto mt-8 max-w-7xl sm:px-6 lg:py-16 lg:px-8">
-  
+
     <h2 class="text-3xl font-extrabold text-gray-900 sm:text-4xl">
       Future Sessions
     </h2>
@@ -53,16 +59,16 @@ get_header();
     <p>Future sessions will be announced here.</p>
 
 
-        <h2 class="pt-16 text-3xl font-extrabold text-gray-900 sm:text-4xl">
+    <h2 class="pt-16 text-3xl font-extrabold text-gray-900 sm:text-4xl">
       Past Sessions
-    </h2>  
-        
-    
+    </h2>
 
-    <div class="mt-6">
+
+
+    <div class="mt-6 hidden">
       <dl class="space-y-8 divide-y divide-gray-200">
 
-      <div class="pt-6 md:grid md:grid-cols-12 md:gap-8">
+        <div class="pt-6 md:grid md:grid-cols-12 md:gap-8">
           <dt class="text-base font-medium text-gray-900 md:col-span-3">
             <h3>February 21, 2021</h3>
 
@@ -71,9 +77,9 @@ get_header();
             <p><a href="https://github.com/rfortherestofus/demystifying-r/blob/28d499cbdb1a76ee733b34fbd78ef3771d30f4b9/report.Rmd">RMarkdown Document Used to Make Report</a></p>
           </dt>
           <dd class="mt-2 md:mt-0 md:col-span-9">
-            
-          <div class="aspect-w-16 aspect-h-9">
-            <iframe src="https://player.vimeo.com/video/514437007?color=6cabdd&byline=0&portrait=0"></iframe>
+
+            <div class="aspect-w-16 aspect-h-9">
+              <iframe src="https://player.vimeo.com/video/514437007?color=6cabdd&byline=0&portrait=0"></iframe>
             </div>
           </dd>
         </div>
@@ -85,7 +91,7 @@ get_header();
 </div>
 
 
-  </main><!-- #main -->
+</main><!-- #main -->
 </div><!-- #primary -->
 
 <?php
