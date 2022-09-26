4cf56be1aa695bfe6ba47a00786385ce1a882c6f
David Keyes <david@rfortherestofus.com>
2022-09-26 11:43:16 -0700

    Add podcast page

---
+++ rru-podcast.php
@@ -0,0 +1,65 @@
+<?php
+/*
+ * Template Name: RRU Podcast
+ */
+
+get_header();
+?>
+
+<div id="primary" class="content-area bb-grid-cell">
+
+
+  <main class="max-w-screen-xl px-4 mx-auto mt-8 sm:px-6">
+
+
+
+    <!-- This example requires Tailwind CSS v2.0+ -->
+    <div class="bg-white">
+
+
+      <main>
+        <div>
+          <!-- Hero card -->
+          <div class="relative">
+
+            <div class="mx-auto max-w-6xl sm:px-6 lg:px-8">
+              <div class="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
+                <div class="absolute inset-0">
+
+                  <div class="absolute inset-0 bg-blue-800 mix-blend-multiply"></div>
+                </div>
+                <div class="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
+                  <h1 class="text-4xl font-bold tracking-tight sm:text-5xl lg:text-6xl">
+                    <span class="block text-white">R for the Rest of Us</span>
+                    <span class="block text-blue-200">Podcast</span>
+
+
+                  </h1>
+                  <p class="mx-auto mt-6 text-xl text-blue-200">
+                    You may think of R as a tool for complex statistical analysis, but it's much more than that. From data visualization to efficient reporting, to improving your workflow, R can do it all. On this podcast, I talk with people about how they use R in unique and creative ways.
+                  </p>
+                  <div class="mx-auto mt-10 max-w-lg sm:max-w-none">
+                    <div class="space-y-4">
+                      <iframe src='https://share.transistor.fm/e/r-for-the-rest-of-us-podcast/playlist' width='100%' height='390' frameborder='0' scrolling='no' seamless='true' style='width:100%; height:390px;'></iframe>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- Logo cloud -->
+
+        </div>
+
+        <!-- More main page content here... -->
+      </main>
+    </div>
+
+
+
+  </main><!-- #main -->
+</div><!-- #primary -->
+
+<?php
+get_footer();
