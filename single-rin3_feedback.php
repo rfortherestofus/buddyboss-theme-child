084d0556d7f7b06a7d124acfbcaabf7f90dc89c9
David Keyes <david@rfortherestofus.com>
2022-09-26 07:03:56 -0700

    Make single feedbacks visible

--- single-rin3_feedback.php
+++ single-rin3_feedback.php
@@ -10,7 +10,7 @@ get_header();
 
 <div id="primary" class="content-area">
 
-    <main class="px-4 mx-auto mt-2 max-w-screen-full visible_to_course_25514">
+    <main class="px-4 mx-auto mt-2 max-w-screen-full">
 
 
 
