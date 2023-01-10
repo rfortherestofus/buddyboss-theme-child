1afc794c5dec945eb9b13e1fd04f40bbb5be3a82
David Keyes <david@rfortherestofus.com>
2023-01-10 14:50:26 -0800

    Work on home page

--- rru-home.php
+++ rru-home.php
@@ -22,33 +22,40 @@ get_header();
 
 
 
-          <main class="mt-16 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6 lg:mt-32">
-            <div class="lg:grid lg:grid-cols-12 lg:gap-8">
-              <div class="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left">
+          <div class="mx-auto max-w-7xl px-4 sm:px-6">
 
-                <h1 class="hidden">R for the Rest of Us</h1>
+            <div>
 
-                <img class="my-8 mx-0" src="../wp-content/themes/buddyboss-theme-child/assets/images/rru-wordmark-wide-dark-blue.svg" alt="R for the Rest of Us">
-                <p class="my-12 text-base text-gray-500 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">Learn to use the most powerful tool for working with data,
-                  <span class="font-bold"> even if you've never coded before.</span>
+
+              <h1 class="hidden">R for the Rest of Us</h1>
+
+              <div class="grid justify-items-center">
+                <img class="my-8 mx-0" src="../wp-content/themes/buddyboss-theme-child/assets/images/rru-wordmark.png" alt="R for the Rest of Us">
+
+                <div class="relative mx-auto w-full rounded-lg shadow-lg lg:max-w-6xl aspect-w-16 aspect-h-9">
+
+<iframe src="https://muse.ai/embed/KsyPoF1?search=0&links=0&logo=0&title=0&autoplay=1&volume=0&style=minimal" frameborder="0" allowfullscreen></iframe>
+
+</div>
+
+                <p class="mt-12 mb-2 text-base text-gray-500 sm:text-3xl text-center">Learn to use the most powerful tool for working with data,
                 </p>
-                <a href="/course/getting-started/" class="px-8 my-8 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-blue-600 border border-transparent rounded-md shadow hover:text-white hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue md:py-4 md:text-lg md:px-10">
-                  Start Free Course
-                </a>
+                <p class="text-base text-gray-500 sm:text-3xl text-center font-bold mt-0"> even if you've never coded before.</p>
+
+
+               
               </div>
+            </div>
+
+
+            <div class="lg:grid lg:grid-cols-12 lg:gap-8">
+      
               <div class="mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
 
 
-                <div class="relative mx-auto w-full rounded-lg shadow-lg lg:max-w-md">
-
-                  <iframe src="https://muse.ai/embed/KsyPoF1?search=0&links=0&logo=0&title=0&autoplay=1&volume=0&style=minimal" width="576" height="324" frameborder="0" allowfullscreen></iframe>
-
-
-
-                </div>
               </div>
             </div>
-          </main>
+          
         </div>
       </div>
 
