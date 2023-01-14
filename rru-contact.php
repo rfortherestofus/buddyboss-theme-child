3c4b3fc7e92a965f444f0dde7db21b84140096d2
David Keyes <david@rfortherestofus.com>
2023-01-13 16:05:30 -0800

    Update rru-contact.php

--- rru-contact.php
+++ rru-contact.php
@@ -10,169 +10,125 @@ get_header();
 
   <main class="mt-2 mx-auto max-w-screen-xl px-4 ">
 
-    <div class="relative bg-white">
-      <div class="absolute inset-0">
-        <div class="absolute inset-y-0 left-0 w-1/2 bg-gray-50"></div>
-      </div>
-      <div class="relative max-w-7xl mx-auto lg:grid lg:grid-cols-5">
-        <div class="bg-gray-50 py-16 px-4 sm:px-6 lg:col-span-2 lg:px-8 xl:pr-12">
-          <div class="max-w-lg mx-auto">
-            <h2 class="text-2xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-3xl sm:leading-9">
-              Get in touch
-            </h2>
-
-            <p>
-            Interested in learning more about online courses, custom training, or consulting? I’d love to talk about helping you start your R journey.
-        </p>
-
-            <dl class="mt-0 text-base leading-6 text-gray-500">
-
-              <div>
-                <dt class="sr-only">Email</dt>
-                <dd class="flex ml-0">
-                  <svg class="flex-shrink-0 h-6 w-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-              </svg>
-                  <span class="ml-2">
-                    <a href="mailto:david@rfortherestofus.com">david@rfortherestofus.com</a>
-                  </span>
-                </dd>
-              </div>
-              <div class="mt-0">
-                <dt class="sr-only">Phone number</dt>
-                <dd class="flex ml-0">
-                  <svg class="flex-shrink-0 h-6 w-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
-              </svg>
-                  <span class="ml-2">
-                    <a href="tel:(503) 395-4385">(503) 395-4385</a>
-                  </span>
-                </dd>
-              </div>
+    <div class="bg-white">
 
 
 
-
-
-            </dl>
-            <p>
-              I'm also on Twitter <a href="https://twitter.com/dgkeyes">@dgkeyes</a> and <a href="https://twitter.com/rfortherest">@rfortherest</a> and on <a href="https://www.linkedin.com/in/dgkeyes/">LinkedIn</a>.
-
-            </p>
+      <!-- Header -->
+      <div>
+        <div class="pt-24 pb-12">
+          <div class="relative z-10 mx-auto max-w-7xl pl-4 pr-8 sm:px-6 lg:px-8">
+            <h1 class="rru-page-title">Get in Touch</h1>
+            <p class="mt-6 max-w-3xl text-xl text-warm-gray-500">Interested in learning more about courses, custom training, or consulting? Let's talk.</p>
           </div>
         </div>
-        <div class="bg-white py-16 px-4 sm:px-6 lg:col-span-3 lg:py-24 lg:px-8 xl:pl-12">
-        <?php echo do_shortcode("[ninja_form id=9]"); ?>
-        
-        </div>
       </div>
-    </div>
 
+      <!-- Contact section -->
+      <section class="relative bg-white" aria-labelledby="contact-heading">
+        <div class="absolute h-1/2 w-full bg-warm-gray-50" aria-hidden="true"></div>
+        
 
-    <!-- <div class="bg-white">
-      <div class="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-24 lg:px-8">
-        <div class="grid grid-cols-1 gap-16">
-          <div class="lg:grid lg:grid-cols-3 lg:gap-8">
-            <h2 class="text-2xl leading-8 font-extrabold text-gray-900 sm:text-3xl sm:leading-9">
-              Get in Touch
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+          <div class="relative bg-white shadow-xl">
+            <h2 id="contact-heading" class="sr-only">Contact us</h2>
 
-            </h2>
-
-
-
-
-
-
-
-
-            <div class="mt-8 grid grid-cols-1 gap-12 sm:grid-cols-2 sm:col-gap-8 sm:row-gap-12 lg:mt-0 lg:col-span-2">
-              <div>
-                <h4 class="text-lg leading-6 font-medium text-gray-900">
-                  Email
-                </h4>
-                <dl class="mt-2 text-base leading-6 text-gray-500">
-                  <div>
-                    <dt class="sr-only">
-                      Email
-                    </dt>
-                    <dd>
-                      <a href="mailto:david@rfortherestofus.com">david@rfortherestofus.com</a>
-                    </dd>
-                  </div>
-
+            <div class="grid grid-cols-1 lg:grid-cols-3">
+              <!-- Contact information -->
+              <div class="relative overflow-hidden bg-gradient-to-b from-blue-500 to-blue-600 py-10 px-6 sm:px-10 xl:p-12">
+                <!-- Decorative angle backgrounds -->
+                <div class="pointer-events-none absolute inset-0 sm:hidden" aria-hidden="true">
+                  <svg class="absolute inset-0 h-full w-full" width="343" height="388" viewBox="0 0 343 388" fill="none" preserveAspectRatio="xMidYMid slice" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M-99 461.107L608.107-246l707.103 707.107-707.103 707.103L-99 461.107z" fill="url(#linear1)" fill-opacity=".1" />
+                    <defs>
+                      <linearGradient id="linear1" x1="254.553" y1="107.554" x2="961.66" y2="814.66" gradientUnits="userSpaceOnUse">
+                        <stop stop-color="#fff"></stop>
+                        <stop offset="1" stop-color="#fff" stop-opacity="0"></stop>
+                      </linearGradient>
+                    </defs>
+                  </svg>
+                </div>
+                <div class="pointer-events-none absolute top-0 right-0 bottom-0 hidden w-1/2 sm:block lg:hidden" aria-hidden="true">
+                  <svg class="absolute inset-0 h-full w-full" width="359" height="339" viewBox="0 0 359 339" fill="none" preserveAspectRatio="xMidYMid slice" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M-161 382.107L546.107-325l707.103 707.107-707.103 707.103L-161 382.107z" fill="url(#linear2)" fill-opacity=".1" />
+                    <defs>
+                      <linearGradient id="linear2" x1="192.553" y1="28.553" x2="899.66" y2="735.66" gradientUnits="userSpaceOnUse">
+                        <stop stop-color="#fff"></stop>
+                        <stop offset="1" stop-color="#fff" stop-opacity="0"></stop>
+                      </linearGradient>
+                    </defs>
+                  </svg>
+                </div>
+                <div class="pointer-events-none absolute top-0 right-0 bottom-0 hidden w-1/2 lg:block" aria-hidden="true">
+                  <svg class="absolute inset-0 h-full w-full" width="160" height="678" viewBox="0 0 160 678" fill="none" preserveAspectRatio="xMidYMid slice" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M-161 679.107L546.107-28l707.103 707.107-707.103 707.103L-161 679.107z" fill="url(#linear3)" fill-opacity=".1" />
+                    <defs>
+                      <linearGradient id="linear3" x1="192.553" y1="325.553" x2="899.66" y2="1032.66" gradientUnits="userSpaceOnUse">
+                        <stop stop-color="#fff"></stop>
+                        <stop offset="1" stop-color="#fff" stop-opacity="0"></stop>
+                      </linearGradient>
+                    </defs>
+                  </svg>
+                </div>
+                
+                <dl class="mt-8 space-y-6">
+                  <dt><span class="sr-only">Phone number</span></dt>
+                  <dd class="flex text-base text-blue-50 ml-0">
+                    <!-- Heroicon name: outline/phone -->
+                    <svg class="h-6 w-6 flex-shrink-0 text-blue-200" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+                    </svg>
+                    <span class="ml-1">(503) 395-4385</span>
+                  </dd>
+                  <dt><span class="sr-only">Email</span></dt>
+                  <dd class="flex text-base text-blue-50 ml-0">
+                    <!-- Heroicon name: outline/envelope -->
+                    <svg class="h-6 w-6 flex-shrink-0 text-blue-200" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+                    </svg>
+                    <span class="ml-1">david@rfortherestofus.com</span>
+                  </dd>
                 </dl>
-              </div>
-              <div>
-                <h4 class="text-lg leading-6 font-medium text-gray-900">
-                  Phone
-                </h4>
-                <dl class="mt-2 text-base leading-6 text-gray-500">
-
-                  <div class="mt-1">
-                    <dt class="sr-only">
-                      Phone number
-                    </dt>
-                    <dd>
-                      <a href="tel:(503) 395-4385">(503) 395-4385</a>
-                    </dd>
-                  </div>
-                </dl>
-              </div>
-              <div>
-                <h4 class="text-lg leading-6 font-medium text-gray-900">
-                  Twitter
-                </h4>
-                <dl class="mt-2 text-base leading-6 text-gray-500">
-                  <div>
-                    <dt class="sr-only">
-                      Twitter
-                    </dt>
-                    <dd>
-                      <a href="https://twitter.com/dgkeyes">@dgkeyes</a>
-                    </dd>
-                  </div>
-
-                </dl>
-              </div>
-              <div>
-                <h4 class="text-lg leading-6 font-medium text-gray-900">
-                  Twitter
-                </h4>
-                <dl class="mt-2 text-base leading-6 text-gray-500">
-
-                  <div class="mt-1">
-                    <dt class="sr-only">
-                      Twitter
-                    </dt>
-                    <dd>
-                      <a href="https://twitter.com/rfortherest">@rfortherest</a>
-                    </dd>
-                  </div>
-                </dl>
-              </div>
-              <div>
-                <h4 class="text-lg leading-6 font-medium text-gray-900">
-                  LinkedIn
-                </h4>
-                <dl class="mt-2 text-base leading-6 text-gray-500">
-
-                  <div class="mt-1">
-                    <dt class="sr-only">
-                      LinkedIn
-                    </dt>
-                    <dd>
-                      <a href="https://www.linkedin.com/in/dgkeyes/">Connect</a>
-                    </dd>
-                  </div>
-                </dl>
+                <ul role="list" class="mt-8 flex space-x-12 invisible">
+                  <li>
+                    <a class="text-blue-200 hover:text-blue-100" href="#">
+                      <span class="sr-only">Facebook</span>
+                      <svg class="h-7 w-7" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd" />
+                      </svg>
+                    </a>
+                  </li>
+                  <li>
+                    <a class="text-blue-200 hover:text-blue-100" href="#">
+                      <span class="sr-only">GitHub</span>
+                      <svg class="h-7 w-7" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clip-rule="evenodd" />
+                      </svg>
+                    </a>
+                  </li>
+                  <li>
+                    <a class="text-blue-200 hover:text-blue-100" href="#">
+                      <span class="sr-only">Twitter</span>
+                      <svg class="h-7 w-7" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                        <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+                      </svg>
+                    </a>
+                  </li>
+                </ul>
               </div>
 
+              <!-- Contact form -->
+              <div class="py-10 px-6 sm:px-10 lg:col-span-2 xl:p-12">
+                <?php echo do_shortcode("[ninja_form id=9]"); ?>
+              </div>
             </div>
           </div>
-
         </div>
-      </div>
-    </div> -->
+      </section>
+
+
+    </div>
+
 
   </main><!-- #main -->
 </div><!-- #primary -->
